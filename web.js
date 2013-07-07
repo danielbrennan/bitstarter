3d68b59e3e25d82ad262dca759f5986d655bc748
Daniel Brennan <dbrennan@gmail.com>
2013-07-07 16:36:48 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html) | buf.toString);
+  var content = fs.readFileSync(index.html) | buf.toString;
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
