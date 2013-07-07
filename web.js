d3f8d0eacb4b03a6b7093cbd2daec5421d75407a
Daniel Brennan <dbrennan@gmail.com>
2013-07-07 17:39:27 +0000

    updated .js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World!');
   var fs = require('fs');
-  fs.readFileSync('/index.html');
+  fs.readFileSync('index.html');
   response.send(buf.toString('utf8',0,26));
 });
 
