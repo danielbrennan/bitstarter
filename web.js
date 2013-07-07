d9589e3a4ff0833e99eb8257525408aabf635a08
Daniel Brennan <dbrennan@gmail.com>
2013-07-07 17:25:43 +0000

    Added web.js udpate

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
 
-var fs = require('fs');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html');
+  var fs = require('fs');
+  fs.readFileSync('/index.html');
   response.send(buf.toString('utf8',0,26));
 };
 
