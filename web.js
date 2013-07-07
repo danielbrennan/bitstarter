956d5339d93fb59b12f91b882c0be19780511cb6
Daniel Brennan <dbrennan@gmail.com>
2013-07-07 17:17:28 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,12 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('index.html');
-
 app.get('/', function(request, response) {
   fs.readFileSync('index.html');
-  response.send(buf.toString('utf8',0,26);
-});
+  response.send(buf.toString('utf8',0,26));
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
